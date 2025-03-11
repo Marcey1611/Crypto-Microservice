0885fad442eb53afdf8262fd959ef1d2c1b995e3
me-223965 <me-223965@rwu.de>
2025-03-11 11:21:17 +0100

    Initial Spring Boot project.

---
+++ cryptoservice/src/main/java/com/projectwork/cryptoservice/CryptoserviceApplication.java
@@ -0,0 +1,13 @@
+package com.projectwork.cryptoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoserviceApplication.class, args);
+	}
+
+}
---
+++ cryptoservice/src/test/java/com/projectwork/cryptoservice/CryptoserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectwork.cryptoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
